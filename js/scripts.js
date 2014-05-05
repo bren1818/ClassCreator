20dd6a5dd65ee70c552a1dcdfac9aafc742cadd9
Brendon Irwin <bren1818@gmail.com>
2014-05-04 21:11:44 -0400

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+Array.prototype.remByVal = function(val) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === val) {
+            this.splice(i, 1);
+            i--;
+        }
+    }
+    return this;
+}
+
 function tab(n){
 	var tabs = "";
 	for(var t =0; t< n; t++){
@@ -22,8 +32,21 @@ $(function(){
 	var code =  $('#generatedCode');
 	var sql = $('#generatedSQL');
 	
+	$('input[name="mode"]').change(function(event){
+		if( $(this).val() == "simple" ){
+			$('#variables').show();
+			$('#formMode').hide();
+		}else{
+			$('#variables').hide();
+			$('#formMode').show();
+		}
+	});
+	
+	
 	
 	button.click(function(event){
+		var mode = $('input[name="mode"]').val();
+		
 		if( className.val() == ""){
 			window.alert("Please enter a class name");
 			return;
@@ -77,7 +100,7 @@ $(function(){
 		}
 		
 		code.val( code.val() + '\r\n\t\t/*Special Functions*/');
-		//load, save, getListBY, toJSON
+		
 		
 		
 		//load function
@@ -88,8 +111,17 @@ $(function(){
 		code.val( code.val() + tab(4) + '}');
 		code.val( code.val() + newLine() );
 		code.val( code.val() + tab(4) + '/*Perform Query*/');
-		
-		
+		code.val( code.val() + tab(4) + 'if( $id != "" ){');
+			code.val( code.val() + tab(5) + '$query = $this->connection->prepare("SELECT * FROM `' + className.val().capitalize() + '` WHERE `id` = :id");');
+			code.val( code.val() + tab(5) + '$query->bindParam(\':id\', $id);');
+			code.val( code.val() + tab(5) + 'if( $query->execute() ){');
+				code.val( code.val() + tab(6) + '$' + className.val() + ' = $query->fetchObject("' + className.val().capitalize() + '");');
+			code.val( code.val() + tab(5) + '}');
+			code.val( code.val() + tab(5) + 'if( is_object( $' + className.val() + ' ) ){');
+				code.val( code.val() + tab(6) + '$' + className.val() + '->setConnection( $this->connection );');
+			code.val( code.val() + tab(5) + '}');
+			code.val( code.val() + tab(5) + 'return $' + className.val()  + ';');
+		code.val( code.val() + tab(4) + '}');
 		code.val( code.val() + tab(3) + '}');
 		code.val( code.val() + '\r\n\t\t}');
 		code.val( code.val() + newLine() );
@@ -101,28 +133,71 @@ $(function(){
 				code.val( code.val() + '\r\n\t\t\t$' + vars[v] + ' = $this->get' + vars[v].capitalize() + '();');
 			}
 		}
-		
 		code.val( code.val() + tab(3) + 'if( $this->connection ){');
 		code.val( code.val() + tab(4) + 'if( $id != "" ){');
+		/*Update Operation*/
 		code.val( code.val() + tab(5) + '/*Perform Update Operation*/' );
+		code.val( code.val() + tab(5) + '$query = $this->connection->prepare("UPDATE  `' + className.val().capitalize() + ' SET ');
+		for(var v=0; v < vars.length; v++){
+			if( vars[v] != "connection" && vars[v] != "errors" && vars[v] != "id" ){
+				code.val( code.val() + '`' + vars[v] + '` = :' + vars[v]  + ' ');
+				if( (v + 1) != vars.length ){ code.val( code.val() + ',' ); }
+			}
+		}
+		code.val( code.val() + 'WHERE `id` = :id");');
+		for(var v=0; v < vars.length; v++){
+			if( vars[v] != "connection" && vars[v] != "errors" && vars[v] != "id" ){
+				code.val( code.val() + tab(5) + '$query->bindParam(\'' + vars[v] + '\', $' + vars[v] + ');');
+			}
+		}
+		code.val( code.val() + tab(5) + '$query->bindParam(\'id\', $id);');
+		code.val( code.val() + tab(5) + 'if( $query->execute() ){');
+			code.val( code.val() + tab(6) + 'return $id;');
+		code.val( code.val() + tab(5) + '}else{');
+			code.val( code.val() + tab(6) + 'return -1;');
+		code.val( code.val() + tab(5) + '}');
 		code.val( code.val() + newLine() );
-		
-		
 		code.val( code.val() + tab(4) + '}else{');
+		/*Insert Operation*/
 		code.val( code.val() + tab(5) + '/*Perform Insert Operation*/' );
-		code.val( code.val() + newLine() );
+		code.val( code.val() + tab(5) + '$query = $this->connection->prepare("INSERT INTO `' + className.val().capitalize() + '` (`id`');
+		if( vars.length > 3 ){ code.val( code.val() + ',' ); }
+		for(var v=0; v < vars.length; v++){
+			if( vars[v] != "id" && vars[v] != "connection" && vars[v] != "errors" ){
+				code.val( code.val() + '`' + vars[v] + '`' );
+				if( (v + 1) != vars.length ){ code.val( code.val() + ',' ); }
+			}
+		}
+		code.val( code.val() + ') VALUES (NULL' );
+		if( vars.length > 3 ){ code.val( code.val() + ',' ); }
+		for(var v=0; v < vars.length; v++){
+			if( vars[v] != "id" && vars[v] != "connection" && vars[v] != "errors" ){
+				code.val( code.val() + ':' + vars[v]  );
+				if( (v + 1) != vars.length ){ code.val( code.val() + ',' ); }
+			}
+		}
+		code.val( code.val() + ');");');
+		
+		for(var v=0; v < vars.length; v++){
+			if( vars[v] != "connection" && vars[v] != "errors" ){
+			code.val( code.val() + tab(5) + '$query->bindParam(\':' +  vars[v] + '\', $' + vars[v] + ');');
+			code.val( code.val() + '\r\n' + tab(5) + 'if( $query->execute() ){');
+			code.val( code.val() + tab(6) + '$this->setId( $this->connection->lastInsertId() );');
+			code.val( code.val() + tab(6) + 'return $this->getId();');
+			code.val( code.val() + tab(5) + '}else{');
+			code.val( code.val() + tab(6) + 'return -1;');
+			code.val( code.val() + tab(5) + '}	');	
+			}
+		}
 		code.val( code.val() + tab(4) + '}');
-		
-		
 		code.val( code.val() + tab(3) + '}');
-		
 		code.val( code.val() + tab(2) + '}');
 		code.val( code.val() + '\r\n');
 		
 		
 		
 		code.val( code.val() + newLine() );
-		//list function
+		//listBy function
 		for(var v=0; v < vars.length; v++){
 			if( vars[v] != "connection" && vars[v] != "errors" ){
 				code.val( code.val() + '\r\n\t\tfunction getListBy' +  vars[v].capitalize() +'($' +  vars[v] +'=null){');
@@ -186,7 +261,39 @@ $(function(){
 		
 		code.val( code.val() + '\r\n\t}\r\n?>');
 		code.trigger('autosize.resize');
+		
+		/**************SQL TABLE CREATION**********************/
+		sql.show();
+		sql.val('CREATE TABLE  `' + className.val().capitalize() + '` (');
+		sql.val( sql.val() + '\r\n`id` INT NULL DEFAULT NULL AUTO_INCREMENT PRIMARY KEY');
+		//remove the original pre-defined vars
+		vars.remByVal("id");
+		vars.remByVal("connection");
+		vars.remByVal("errors");
+		
+		
+		if( vars.length > 1 ){ sql.val( sql.val() + ','); }
+		if( mode == "simple" ){
+			for(var v=0; v < vars.length; v++){
+				sql.val( sql.val() + '\r\n' + '`' + vars[v] + '` VARCHAR( 55 )');
+				if( (v + 1) != vars.length ){ sql.val( sql.val() + ',' ); }
+			}
+		}
+		sql.val( sql.val() + '\r\n);');
+		
+		/*
+		CREATE TABLE  `test` (
+		`id` INT NULL DEFAULT NULL AUTO_INCREMENT PRIMARY KEY ,
+		`varchar` VARCHAR( 55 ) NOT NULL ,
+		`integer` INT NOT NULL ,
+		`longtext` LONGTEXT NOT NULL
+		`a` TIMESTAMP NOT NULL ,
+		`bool` BOOLEAN NOT NULL
+		);
+		*/
+		sql.trigger('autosize.resize');
 	});
+	
 	$('textarea').autosize();   
 
 });
